6c349a45c6d6cb6d4f6aa3250308c99b71800e21
Karen Gomes <karen.nayara@audora.com.br>
2018-11-19 09:35:00 -0300

    armazenamento sequencial: classe Vetor

---
+++ src/com/karenngomes/ed/Vetor.java
@@ -0,0 +1,36 @@
+package com.karenngomes.ed;
+
+import java.util.Arrays;
+
+public class Vetor {
+	private Aluno[] alunos = new Aluno[100];
+	
+	public void adiciona(Aluno aluno) {
+		
+	}
+	
+	public void adiciona(int posicao, Aluno aluno) {
+		
+	}
+	
+	public Aluno pega(int posicao) {
+		return null;
+	}
+	
+	public void remove(int posicao) {
+		
+	}
+	
+	public boolean contem(Aluno aluno) {
+		return false;
+	}
+	
+	public int tamanho() {
+		return 1;
+	}
+	
+	public String toString() {
+		return Arrays.toString(alunos);
+	}
+	
+}
